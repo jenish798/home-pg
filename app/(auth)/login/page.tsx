9c7f27a4e79c399b90163abcd9194704096e3db3
jenish798 <solomonjenish@gmail.com>
2024-03-20 15:37:29 +0530

    Keedrive login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,15 +1,32 @@
 import Image from 'next/image'
 import type { Metadata } from 'next'
+import React,{useState} from 'react';
+// import phoneOtpForm from '@/components/login/login';
 
 export const metadata:Metadata =  {
     title: 'Login | Keedriver',
     description: 'Login Page',
 }
-  
-export default function Login() {
-  return (
-    <main className="flex mx-auto">
-      Hello Login
-    </main>
-  )
+function LoginPage() {
+return(
+<>
+<div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <form className="max-w-md w-full p-8 bg-white shadow-md rounded-lg">
+          <h1 className="text-3xl font-semibold text-center mb-6">Welcome to KeeDrive</h1>
+          <p className="text-center mb-4">Hi.</p>
+          <p className="text-center mb-4">Let's get started</p>
+          <div className="mb-4">
+            <label htmlFor="mobile" className="block mb-2 text-sm font-medium text-gray-700">Enter Mobile Number</label>
+            <input type="tel" id="mobile" name="mobile" className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-red-500" />
+          </div>
+          <button type="submit" className="w-full bg-primary text-white py-2 rounded-lg hover:bg-red-600 focus:outline-none focus:bg-red-600">Get Otp</button>
+          </form>
+      </div>
+  </>
+)
 }
+
+
+  
+
+export default LoginPage;
---
+++ components/login/login.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const phoneOtpForm = () => {
+  return
+    <div>phoneOtpForm</div>
+  
+}
+
+export default phoneOtpForm;
